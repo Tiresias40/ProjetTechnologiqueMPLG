35d616e8792cee42356d04d71f8f0a7cac4b7f51
Mathieu Perroy <mathieu.perroy@gmail.com>
2017-03-17 13:39:51 +0100

    Methode initiales et registrer

--- Bus.java
+++ Bus.java
@@ -1,40 +1,26 @@
-package Bus;
+package projet;
 import java.io.*;
 import java.net.*;
+
 import javax.json.*;
 
 public class Bus {
-	public void main(String args[]) throws Exception{
-		ServerSocket serverSocket = new ServerSocket(8080); // SE METTRE D'ACCORD !!!
+		ServerSocket socketBus = null; 
+		PrintWriter printW;
+		BufferedReader reader;
 		
-		while(true){
-			Socket socket = serverSocket.accept();
-			
-			InputStream in = socket.getInputStream();
-			OutputStream out = socket.getOutputStream();
-			
-			String[] sender_tab = new String[5]; // A modifier : peut-être un tableau pour chaque type de device.
-			
-			int int_sender_class = in.read();
-			int int_sender_name = in.read();
-			
-			// A convertir !!!
-			
-			/*			
-			if (entier < table.length && entier >= 0 ){ // on vérifie que l'id du client
-				if(in_out==0){ // Si le client entre :
-					if (table[entier] == 0){ // Si le client n'est pas présent
-						table[entier] = 1;
-						sortie.write(1);
-					}else // Si le client est déjà présent.
-						sortie.write(0);
-				}else // Si le client sort
-					table[entier] = 0;
-			}
-			*/
-			
-			serverSocket.close();
-			socket.close();
+		public Bus( int numPort) throws IOException {
+		  socketBus = new ServerSocket(numPort);
+    }
+		
+		public Bus() throws IOException {
+		  this(7182);
 		}
-	}
+		
+		JsonObject request (JsonObject jsonObj) {
+		  printW.println(jsonObj);
+		  JsonReader jreader = Json.createReader(reader);
+		  jsonObj = (JsonObject) jreader.read();
+		  return jsonObj;
+		}
 }
--- Sender.java
+++ Sender.java
@@ -1,20 +1,27 @@
-package Bus;
+package projet;
+
+import javax.json.*;
 
 public class Sender {
-	String sender_class;
-	String sender_name;
-	int sender_id;
+	private String sender_class;
+	private String sender_name;
+	private int sender_id;
 	
-	public Sender(String sender_class, String sender_name){
+	public Sender(String sender_class, String sender_name) {
 		this.sender_class = sender_class;
 		this.sender_name = sender_name;
 		this.sender_id = -1;
 	}
 	
-	public int register(String sender_class, String sender_name){ // return id
-		return 0;
+	public void register(String sender_class, String sender_name) { 
+	  JsonObjectBuilder request = Json.createObjectBuilder();
+	  request.add("sender_class", sender_class);
+	  request.add("sender_name", sender_name);
+	  request.build();
+		
 	}
-	public int deregister(int sender_id){ // entier de confirmation
+	
+	public int deregister(int sender_id) { // entier de confirmation
 		return 0;
 	}
 }
