23ccf3043da9cccce235de5d50f71641f9b51473
Lucas Ghibert <lghibert@emi.u-bordeaux1.fr>
2017-04-05 15:08:28 +0200

    ajout fonction get_last, fin fonction get dans Sender.java, modif indentation Bus.java (aucune modif importante)

--- Bus.java
+++ Bus.java
@@ -1,51 +1,57 @@
 package projet;
+
 import java.io.*;
 import java.net.*;
 
 import javax.json.*;
 
 public class Bus {
-		private ServerSocket socketBus = null; 
-		private PrintWriter printW;
-		private BufferedReader reader;
-		// Tableau contenant toutes les infos sur les capteurs connectés
-		private Sender[] tabSender = new Sender[10];
-		
-		
-		//num de port 7182
-		public Bus(int numPort) throws IOException {
-		  socketBus = new ServerSocket(numPort);
-    }
-		
-		public Bus() throws IOException {
-		  this(7182);
+	private ServerSocket socketBus = null;
+	private PrintWriter printW;
+	private BufferedReader reader;
+	// Tableau contenant toutes les infos sur les capteurs connectés
+	private Sender[] tabSender = new Sender[10];
+
+	// num de port 7182
+	public Bus(int numPort) throws IOException {
+		socketBus = new ServerSocket(numPort);
+	}
+
+	public Bus() throws IOException {
+		this(7182);
+	}
+
+	JsonObject request(JsonObject jsonObj) {
+		printW.println(jsonObj);
+		JsonReader jreader = Json.createReader(reader);
+		jsonObj = (JsonObject) jreader.read();
+		jreader.close();
+		return jsonObj;
+	}
+
+	public JsonObject list(String sender_class, String sender_name) {
+		Sender[] results = new Sender[tabSender.length];
+		for (int count = 0; count < tabSender.length; count++) {
+			if (sender_class != null
+					&& tabSender[count].getSender_class().equals(sender_class)) {
+				if (sender_name != null
+						&& tabSender[count].getSender_name()
+								.equals(sender_name)) { // Les deux
+					results[count] = tabSender[count];
+				} else { // Que class
+					results[count] = tabSender[count];
+				}
+			} else {
+				if (sender_name != null
+						&& tabSender[count].getSender_name()
+								.equals(sender_name)) { // Que name
+					results[count] = tabSender[count];
+				} else { // Tout envoyer (aucun des deux attributs)
+					results[count] = tabSender[count];
+				}
+			}
 		}
-		
-		JsonObject request (JsonObject jsonObj) {
-		  printW.println(jsonObj);
-		  JsonReader jreader = Json.createReader(reader);
-		  jsonObj = (JsonObject) jreader.read();
-		  return jsonObj;
-		}
-		
-		public JsonObject list(String sender_class, String sender_name) {
-		  Sender[] results = new Sender[tabSender.length];
-		  for(int count = 0; count < tabSender.length; count ++) {  
-		    if(sender_class != null && tabSender[count].getSender_class().equals(sender_class)) {
-		      if(sender_name != null && tabSender[count].getSender_name().equals(sender_name)){ // Les deux
-		        results[count] = tabSender[count];
-		      } else{ // Que class
-		        results[count] = tabSender[count];
-		      }
-		    } else {
-		      if(sender_name != null && tabSender[count].getSender_name().equals(sender_name)){ // Que name
-		        results[count] = tabSender[count];
-		      } else { // Tout envoyer (aucun des deux attributs)
-		        results[count] = tabSender[count];
-		      }
-		    }
-		  }
-		  // Convertir Sender[] en JsonObject (Array ?)
-		  return ;
-		}
+		// Convertir Sender[] en JsonObject (Array ?)
+		return;
+	}
 }
--- Sender.java
+++ Sender.java
@@ -1,5 +1,6 @@
 package projet;
 
+import java.io.*;
 import javax.json.*;
 
 public class Sender {
@@ -7,103 +8,116 @@ public class Sender {
 	private String sender_name;
 	private int sender_id;
 	private Bus bus; // Peut-être à supprimer ?
-  private int last_id; // Id du dernier message
-	
+	private int last_id; // Id du dernier message
+
 	public Sender(String sender_class, String sender_name, Bus bus) {
 		this.sender_class = sender_class;
 		this.sender_name = sender_name;
 		this.sender_id = -1;
 		this.bus = bus;
 	}
+
 	public String getSender_class() {
-    return sender_class;
-  }
-  public void setSender_class(String sender_class) {
-    this.sender_class = sender_class;
-  }
-  public String getSender_name() {
-    return sender_name;
-  }
-  public void setSender_name(String sender_name) {
-    this.sender_name = sender_name;
-  }
-	
-	public void printError(int error){
-	  switch(error){
-	  case 400:
-	    System.out.println("Bad Request - Your request sucks.");
-	    break;
-	  case 401:
-	    System.out.println("Unauthorized - Your API key is wrong.");
-	  case 403:
-	    System.out.println("Forbidden - The kitten requested is hidden for administrators only.");
-	  case 404:
-	    System.out.println("Not Found - The specified kitten could not be found.");
-	  case 405:
-	    System.out.println("Method Not Allowed - You tried to access a kitten with an invalid method.");
-	  case 406:
-	    System.out.println("Not Acceptable - You requested a format that isn't json.");
-	  case 410:
-	    System.out.println("Gone - The kitten requested has been removed from our servers.");
-	  case 418:
-	    System.out.println("I'm a teapot.");
-	  case 429:
-	    System.out.println("Too many requests - You're requesting too many kittens ! Slow down !");
-	  case 500:
-	    System.out.println("Internal server error - We had a problem with our server. Try again later.");
-	  case 503:
-	    System.out.println("Service Unavailable - We're temporarily offline for maintenance. Please try again later.");
-	  }
+		return sender_class;
 	}
-	
-	public void register(String sender_class, String sender_name) { 
-	  //initialisation du json à envoye printError(error);r
-	  JsonObjectBuilder requestBuild = Json.createObjectBuilder();
-	  requestBuild.add("type", "register");
-	  requestBuild.add("sender_class", sender_class);
-	  requestBuild.add("sender_name", sender_name);
-	  JsonObject request = requestBuild.build(); 
-	  //retour du serveur + mise à jour du champ sender_id
-		JsonObject answer = bus.request(request);
-		String ack = answer.getString("resp");
-		if(ack.equals("ok")) 
-		  sender_id = answer.getInt("sender_id");
-		else {
-		  int error = answer.getInt("error_id");
-		  printError(error);
+
+	public void setSender_class(String sender_class) {
+		this.sender_class = sender_class;
+	}
+
+	public String getSender_name() {
+		return sender_name;
+	}
+
+	public void setSender_name(String sender_name) {
+		this.sender_name = sender_name;
+	}
+
+	public void printError(int error) {
+		switch (error) {
+		case 400:
+			System.out.println("Bad Request - Your request sucks.");
+			break;
+		case 401:
+			System.out.println("Unauthorized - Your API key is wrong.");
+		case 403:
+			System.out
+					.println("Forbidden - The kitten requested is hidden for administrators only.");
+		case 404:
+			System.out
+					.println("Not Found - The specified kitten could not be found.");
+		case 405:
+			System.out
+					.println("Method Not Allowed - You tried to access a kitten with an invalid method.");
+		case 406:
+			System.out
+					.println("Not Acceptable - You requested a format that isn't json.");
+		case 410:
+			System.out
+					.println("Gone - The kitten requested has been removed from our servers.");
+		case 418:
+			System.out.println("I'm a teapot.");
+		case 429:
+			System.out
+					.println("Too many requests - You're requesting too many kittens ! Slow down !");
+		case 500:
+			System.out
+					.println("Internal server error - We had a problem with our server. Try again later.");
+		case 503:
+			System.out
+					.println("Service Unavailable - We're temporarily offline for maintenance. Please try again later.");
 		}
 	}
-	
-	public String deregister(int sender_id) {
-	  //initialisation du json à envoyer
-	  JsonObjectBuilder requestBuild = Json.createObjectBuilder();
-	  requestBuild.add("type", "deregister");
-	  requestBuild.add("sender_id", sender_id);
-	  JsonObject request = requestBuild.build();
-	  //retour serveur
-	  JsonObject answer = bus.request(request);
-	  String ack = answer.getString("resp");
-	  if(ack.equals("error")){
-	    int error = answer.getInt("error_id");
-	    printError(error);
-	  }
-	  return ack;
+
+	public void register() {
+		// initialisation du json à envoye printError(error);r
+		JsonObjectBuilder requestBuild = Json.createObjectBuilder();
+		requestBuild.add("type", "register");
+		requestBuild.add("sender_class", this.sender_class);
+		requestBuild.add("sender_name", this.sender_name);
+		JsonObject request = requestBuild.build();
+		// retour du serveur + mise à jour du champ sender_id
+		JsonObject answer = bus.request(request);
+		String ack = answer.getString("resp");
+		if (ack.equals("ok"))
+			this.sender_id = answer.getInt("sender_id");
+		else {
+			int error = answer.getInt("error_id");
+			printError(error);
+		}
 	}
-	
+
+	public String deregister() {
+		// initialisation du json à envoyer
+		JsonObjectBuilder requestBuild = Json.createObjectBuilder();
+		requestBuild.add("type", "deregister");
+		requestBuild.add("sender_id", this.sender_id);
+		JsonObject request = requestBuild.build();
+		// retour serveur
+		JsonObject answer = bus.request(request);
+		String ack = answer.getString("resp");
+		if (ack.equals("error")) {
+			int error = answer.getInt("error_id");
+			printError(error);
+		}
+		return ack;
+	}
+
 	public void send(JsonObject content) {
-	  JsonObjectBuilder requestBuild = Json.createObjectBuilder();
-	  requestBuild.add("type", "send");
-	  requestBuild.add("sender_id", sender_id);
-	  requestBuild.add("content", content);
-	  JsonObject request = requestBuild.build();
-	  //retour serveur
-	  JsonObject answer = bus.request(request);
-	  String ack = answer.getString("resp");
-	  if(ack.equals("error")) {
-	    int error = answer.getInt("error_id");
-      printError(error);
-	  }  
+		JsonObjectBuilder requestBuild = Json.createObjectBuilder();
+		requestBuild.add("type", "send");
+		requestBuild.add("sender_id", this.sender_id);
+		requestBuild.add("content", content);
+		JsonObject request = requestBuild.build();
+		// retour serveur
+		JsonObject answer = bus.request(request);
+		String ack = answer.getString("resp");
+		if (ack.equals("error")) {
+			int error = answer.getInt("error_id");
+			printError(error);
+		}
 	}
+
 	public void get(int sender_id, int msg_id) {
 		JsonObjectBuilder requestBuild = Json.createObjectBuilder();
 		requestBuild.add("type", "get");
@@ -116,13 +130,32 @@ public class Sender {
 		if (ack.equals("error")) {
 			int error = answer.getInt("error_id");
 			printError(error);
-		}else{
+		} else {
 			// Afficher contenu message
-			// Temporaire
+			// Temporaire pour GPS
 			int lat = answer.getInt("lat");
 			int lng = answer.getInt("lng");
-			System.out.println("lat : "+lat+" lng : "+lng);
+			System.out.println("lat : " + lat + " lng : " + lng);
+		}
+	}
+	
+	public void get_last(int sender_id){
+		JsonObjectBuilder requestBuild = Json.createObjectBuilder();
+		requestBuild.add("type", "get_last");
+		requestBuild.add("sender_id", sender_id);
+		JsonObject request = requestBuild.build();
+		// retour serveur
+		JsonObject answer = bus.request(request);
+		String ack = answer.getString("resp");
+		if (ack.equals("error")) {
+			int error = answer.getInt("error_id");
+			printError(error);
+		} else {
+			// Afficher contenu message
+			// Temporaire pour GPS
+			int lat = answer.getInt("lat");
+			int lng = answer.getInt("lng");
+			System.out.println("lat : " + lat + " lng : " + lng);
 		}
 	}
 }
-
